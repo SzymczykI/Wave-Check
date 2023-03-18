0dc084c2cc0076b8e10d35d6367d0b020fdad98c
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2023-03-18 17:26:12 +0100

    reading dataset, extracting hmax and api endpoint on the server

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import xarray as xr
-import numpy as np
 
 app = Flask(__name__)
 CORS(app)
 
 ds = xr.open_dataset('waves_2019-01-01.nc')
 
+@app.route('/data', methods=['POST'])
+def get_data():
+    # Getting the coordinates of the selected point from the request
+    lat = request.json['lat']
+    lon = request.json['lon']
+
+    # finding maximum wave height at the selected point
+    point_data = ds.sel(latitude=lat, longitude=lon, method='nearest')
+    hmax = point_data['hmax']
+    max_wave_height = hmax.max().item()
+
+    return jsonify({'max_wave_height': max_wave_height})
